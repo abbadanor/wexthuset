f93e022a69805460593c37124f346285fc9f6aa1
Adam Nord <adam.nord@abbgymnasiet>
2022-09-09 14:50:53 +0200

    re-added router

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,76 +1,127 @@
 <script setup>
 import { ref } from 'vue';
-const wateringTime = ref(4)
+import { CronJob, CronTime } from 'cron'
+import axios from 'axios'
+
+const cronTimeParser = (ct) => {
+    const intervalDictionary = { 1: "minut", 2: "timme", 3: "dag" }
+    if (ct.includes('/')) {
+        const intervalArray = ct.split(' ')
+        for (let i in intervalArray) {
+            if (intervalArray[i].includes('/')) {
+                const timeInterval = intervalArray[i].split('/')[1]
+                return { interval: intervalDictionary[i], time: timeInterval }
+            }
+        }
+    }
+}
+
 const fanSpeed = ref(50)
-const frequencies = [
-    {id:0,name:"Dag"},
-    {id:0,name:"Timme"},
-    {id:0,name:"Minut"},
-]
+const frequencies = ['minut', 'timme', 'dag']
+const cronTime = ref("0 */2 * * * *")
+const userTime = ref(cronTimeParser(cronTime.value).time)
+const userInterval = ref(cronTimeParser(cronTime.value).interval)
+const job = new CronJob(
+    cronTime.value,
+    function () {
+        console.log('You will see this message every second');
+    },
+    null,
+    true,
+    'Europe/Stockholm'
+);
+
+const generateCronTime = (time, interval) => {
+    switch (interval) {
+        case 'minut':
+            return `0 */${time} * * * *`
+        case 'timme':
+            return `0 0 */${time} * * *`
+        case 'dag':
+            return `0 0 0 */${time} * *`
+    }
+}
+
+const setCronTime = () => {
+    cronTime.value = generateCronTime(userTime.value, userInterval.value)
+    const newCronTime = new CronTime(cronTime.value)
+    job.setTime(newCronTime)
+}
+
+const parseCronTime = () => {
+    console.log(cronTimeParser(cronTime.value))
+}
+
+const logNextDates = () => {
+    console.log(job.nextDates().c)
+    // const { year, month, day, hour, minute,second } = job.nextDates().c
+    // print(dayjs(year, month, day, hour, minute, second).fromNow())
+}
+
+const callApi = async () => {
+    try {
+        const resp = await axios.get('https://api.simsva.se/wexteras/data?id=1');
+        console.log(resp.data);
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+}
+ // onMounted(() => {
+ //     dayjs.extend(relativeTime)
+ //  })
+
 </script>
+
 <template>
-    <input type="checkbox" id="my-modal" class="modal-toggle" />
-    <div class="modal">
-        <div class="modal-box">
-            <h3 class="font-bold text-lg">Hur ofta vill du vattna?</h3>
-            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!
-            </p>
-            <div class="form-control">
-                <div class="input-group">
-                    <span>Var</span>
-                    <input type="text" placeholder="10" class="input input-bordered" />
-                    <span>:e</span>
-                    <select v-model="frequency" class="select select-bordered">
-                        <option v-for="f in frequencies" v-bind:value="{id:f.id, text:f.name}">
-                        {{f.name}}
-                    </option>
-                    </select>
-                </div>
-            </div>
-            <div class="modal-action">
-                <label for="my-modal" class="btn">Yay!</label>
-            </div>
-        </div>
-    </div>
-    <div class="flex flex-col p-3">
-        <h1 class="text-4xl font-bold">Wexthuset</h1>
-        <div>
-            <h1 class="text-3xl">Home</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, tempora alias. Placeat eius qui a fuga
-                quisquam, eum enim unde aliquid dolores quaerat inventore voluptatem iure pariatur ab laborum?
-                Inventore?</p>
-        </div>
-        <div class="flex flex-col">
-            <div class="card w-full bg-base-200 shadow-xl mt-3">
-                <div class="card-body">
-                    <h2 class="card-title">Bevattning</h2>
-                    <p>Växthuset kommer vattnas om {{ wateringTime }} minuter</p>
-                </div>
-                <div class="card-actions justify-end">
-                    <label for="my-modal" class="btn modal-button">open modal</label>
-                </div>
-            </div>
-            <div class="card w-full bg-base-200 shadow-xl mt-3">
-                <div class="card-body">
-                    <h2 class="card-title">Styr fläkten</h2>
-                    <div class="flex">
-                        <input type="range" v-model="fanSpeed" min="0" max="100" class="range range-primary" />
-                        <span class="font-bold ml-4 whitespace-nowrap">{{ fanSpeed * 100 }} RPM</span>
+        <div class="flex justify-center">
+            <input type="checkbox" id="my-modal" class="modal-toggle" />
+            <div class="flex flex-col p-3">
+                <h1 class="text-4xl font-bold">Wexthuset</h1>
+                <div class="flex flex-col">
+                    <div class="card w-full bg-base-200 shadow-xl mt-3">
+                        <div class="card-body">
+                            <h2 class="card-title">Bevattning</h2>
+                            <p>{{userTime}}</p>
+                            <p>{{userInterval}}</p>
+                            <div class="input-group">
+                                <span>Var</span>
+                                <input id="time" v-model="userTime" type="text" placeholder="10"
+                                    class="input input-bordered" />
+                                <span>:e</span>
+                                <select id="interval" v-model="userInterval" class="select select-bordered">
+                                    <option v-for="f in frequencies" v-bind:value="f">
+                                        {{f}}
+                                    </option>
+                                </select>
+                                <button @click="setCronTime" class="btn">Go</button>
+                            </div>
+                            <p>{{cronTime}}</p>
+                            <button @click="logNextDates" class="btn">Get next dates</button>
+                            <button @click="parseCronTime" class="btn">Crontime parser</button>
+                            <button @click="callApi" class="btn">Get api</button>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="card w-full bg-base-200 shadow-xl mt-3">
-                <div class="card-body">
-                    <h2 class="card-title">Mätvärden</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div class="flex">
-                        <div class="radial-progress text-sky-600" style="--value:70;">70%</div>
-                        <div class="radial-progress text-fuchsia-700" style="--value:70;">70%</div>
+                    <div class="card w-full bg-base-200 shadow-xl mt-3">
+                        <div class="card-body">
+                            <h2 class="card-title">Styr fläkten</h2>
+                            <div class="flex">
+                                <input type="range" v-model="fanSpeed" min="0" max="100" class="range range-primary" />
+                                <span class="font-bold ml-4 whitespace-nowrap">{{ fanSpeed * 100 }} RPM</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card w-full bg-base-200 shadow-xl mt-3">
+                        <div class="card-body">
+                            <h2 class="card-title">Mätvärden</h2>
+                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <div class="flex">
+                                <div class="radial-progress text-sky-600" style="--value:70;">70%</div>
+                                <div class="radial-progress text-fuchsia-700" style="--value:70;">70%</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </template>
-<style>
-</style>
